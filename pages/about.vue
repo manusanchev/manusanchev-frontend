0299aef989e38de0f29676d5cf4e926db07da048
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-16 21:06:21 +0100

    about page

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ms-home">
+   sadwsa
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+</style>
