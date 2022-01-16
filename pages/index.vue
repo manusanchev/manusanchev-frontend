5d8d229f8e59e47513122ba2fab83e318e386d21
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-16 18:31:19 +0100

    analitycs

--- pages/index.vue
+++ pages/index.vue
@@ -42,11 +42,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-export default defineComponent({
-    setup() {
-        
-    },
-})
+export default defineComponent({})
 </script>
 
 <style scoped>
