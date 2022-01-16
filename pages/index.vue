45a68ff7f821606e543b686d832b27239140c701
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-16 13:58:48 +0100

    upload

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+Bienvenidos a mi web
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
