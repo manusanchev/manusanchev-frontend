3b8d42d92718e72d3af394c28f80b0feaa61f836
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-16 20:06:07 +0100

    d

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,11 +1,15 @@
 import { defineNuxtPlugin } from '#app'
 import VueGtag from 'vue-gtag-next'
-import {useRouter} from "nuxt3/dist/pages/runtime/composables";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(VueGtag, {
-        property: {
-            id: 'UA-186191303-1'
-        }
-    })
+    const router = nuxtApp.vueApp.$nuxt.$router;
+    nuxtApp.vueApp.use(
+        VueGtag,
+        {
+            config: {
+                id: 'UA-186191303-1'
+            },
+        },
+        router
+    );
 })
