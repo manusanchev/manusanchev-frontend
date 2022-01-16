c3fe0f507601beaa27e029165541d8aa2c10b5ae
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-16 18:03:22 +0100

    google

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    meta: {
+        title: 'Frontend Developer - Manuel Sanchez',
+        meta: [
+            {name: "google-site-verification" ,content: "dPbYI3pAlXBeum68Tb_zxLK_kOmWy5BhkG5HlZvzXGc"},
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Desarrollador web con experiencia en aplicaciones web zero downtime, aplicando clean architecture y buenas practicas con SOLID'
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
     css: [
         '@/assets/main.css',
     ],
@@ -9,5 +23,6 @@ export default defineNuxtConfig({
         postcss: {
             postcssOptions: require('./postcss.config.ts'),
         },
-    }
+    },
+
 })
