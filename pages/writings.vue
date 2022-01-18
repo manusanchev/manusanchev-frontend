04f74667c845d4dc9e8826caa9d0a7e89d34b160
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-01-18 09:56:18 +0100

    favicon

--- pages/writings.vue
+++ pages/writings.vue
@@ -7,7 +7,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-export default defineComponent({})
+export default defineComponent({
+  head: {
+    title: "✅ Experiencias en desarrollo web | Manuel Sanchez",
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: '"Si estas aprendiendo a programar o ya tienes experiencia senior, te invito a leer lo que escribo, quizas aprendamos algo nuevo juntos. ✅ Hablo sobre Frontend ✅"'
+      },
+    ],
+  }
+})
 </script>
 
 <style scoped>
