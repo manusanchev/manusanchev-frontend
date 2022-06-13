481944d070798425dc693285fa01d95b13aaca8e
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-06-14 00:09:02 +0200

    remove hggithub

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,16 +1,12 @@
 <template>
   <footer class="mb-16 flex flex-col items-center justify-center gap-4 text-slate-400">
-    <h3 class="text-lg md:text-xl">Diseñado en <span>Figma</span> y desarrollado con <span>Vuejs</span></h3>
-    <a href="https://www.github.com/manusanchev" target="_blank">
-      <GithubIcon class="h-10 w-10 md:w-14 md:h-14 hover:text-slate-300" />
-    </a>
-    <i class="italic text-lg">manusanchev &copy; {{ new Date().getFullYear() }}</i>
+    <h3 class="text-lg md:text-xl">
+      Diseñado en <span>Figma</span> y desarrollado con <span>Vuejs</span>
+    </h3>
+    <i class="text-lg italic">manusanchev &copy; {{ new Date().getFullYear() }}</i>
   </footer>
 </template>
 
-<script setup lang="ts">
-import GithubIcon from "@components/icons/GithubIcon.vue";
-</script>
 
 <style scoped>
 span {
