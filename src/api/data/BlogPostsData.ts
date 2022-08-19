b71ca37673cc73aef2f81ad636c7934674bae4e3
Manuel Sánchez Velázquez <49983557+manusanchev@users.noreply.github.com>
2022-08-19 18:25:35 +0200

    add new post to the list

--- src/api/data/BlogPostsData.ts
+++ src/api/data/BlogPostsData.ts
@@ -5,4 +5,8 @@ export const blogPostsData: IBlogSectionItem[] = [
     title: "Mejora tu SEO y consigue más visitas en tu web parte 1",
     url: "https://mamby.com/p/mejora-tu-seo-y-consigue-mas-visitas-en-tu-web-parte-1-cf0e98e0",
   },
+  {
+    title: "Despliega tu aplicación web en Digital Ocean con Github Actions [Configuración de nuestro VPS]",
+    url: "https://mamby.com/p/despliega-tu-aplicacion-web-en-digital-ocean-con-github-actions-configuracion-de-nuestro-vps-33359d65",
+  },
 ];
