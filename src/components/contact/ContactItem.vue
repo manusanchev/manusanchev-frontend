70c01f657bd4eaff5b316a0f3bda2e2feb5de344
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-10-16 00:17:37 +0200

    solve icons contact section

--- src/components/contact/ContactItem.vue
+++ src/components/contact/ContactItem.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="mx-auto flex items-end gap-4">
-<!--    <component class="h-16 w-16 md:h-20 md:w-20 fill-current text-white" :is="item.contactType" />-->
+    <component
+      class="h-16 w-16 fill-current text-white md:h-20 md:w-20"
+      :is="contactIconMap.get(item.contactType)"
+    />
     <div>
-      <a :href="item.url" :alt="item.url" target="_blank" :data-screen-name="item.additionalData" class="block text-center md:text-lg text-white border py-1.5 border-white rounded hover:text-slate-900 hover:bg-white"
+      <a
+        :href="item.url"
+        :title="item.url"
+        target="_blank"
+        :data-screen-name="item.additionalData"
+        class="block rounded border border-white py-1.5 text-center text-white hover:bg-white hover:text-slate-900 md:text-lg"
         >Enviar mensaje</a
       >
       <p class="text-white md:text-lg" v-html="item.hintTime" />
@@ -11,7 +19,7 @@
 </template>
 
 <script setup lang="ts">
-import { ContactSectionItem } from "@services/portfolio/types/ContactSectionItem";
+import { contactIconMap, ContactSectionItem } from "@services/portfolio/types/ContactSectionItem";
 
 defineProps<{
   item: ContactSectionItem;
--- src/services/portfolio/types/ContactSectionItem.ts
+++ src/services/portfolio/types/ContactSectionItem.ts
@@ -1,12 +1,22 @@
-export enum ContactIcon {
+import LinkedinIcon from "@components/icons/LinkedinIcon.vue";
+import TwitterIcon from "@components/icons/TwitterIcon.vue";
+import EmailIcon from "@components/icons/EmailIcon.vue";
+
+export enum ContactType {
   LINKEDIN = 1,
   TWITTER = 2,
   EMAIL = 3,
 }
 
 export interface ContactSectionItem {
-  contactType: ContactIcon;
+  contactType: ContactType;
   url: string;
   additionalData?: string | undefined;
   hintTime: string;
 }
+
+export const contactIconMap: Map<ContactType, any> = new Map<ContactType, any>([
+  [ContactType.LINKEDIN, LinkedinIcon],
+  [ContactType.TWITTER, TwitterIcon],
+  [ContactType.EMAIL, EmailIcon],
+]);
