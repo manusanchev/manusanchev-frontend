9e94e0638487099363178c17fa6c9acf6aa302e6
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2022-10-16 00:29:09 +0200

    remove unused paths and add new ones

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from "./App.vue";
 import { ViteSSG } from "vite-ssg/single-page";
-import "@src/assets/style/index.css";
-import Container from "@src/utils/container";
+import "@assets/style/index.css";
+import Container from "@utils/container";
 
 export const createApp = ViteSSG(App, (context) => {
   const container = new Container();
