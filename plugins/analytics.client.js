eddacde07ec7c09503b91eecc4dd639650b3633f
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-10-09 11:11:44 +0200

    Redesign with fonts and colors

--- plugins/analytics.client.js
+++ plugins/analytics.client.js
@@ -1,5 +1,13 @@
-import { inject } from '@vercel/analytics'
+import { inject } from "@vercel/analytics";
 
-export default defineNuxtPlugin(() => {
-  inject();
-})
+export default defineNuxtPlugin({
+  name: "analytics",
+  parallel: true,
+  async setup() {
+    if (process.env.NODE_ENV !== "production") {
+      return;
+    }
+
+    inject();
+  },
+});
