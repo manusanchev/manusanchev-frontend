424a78f6214d8a9183b14e067dcb950c7f83a3ff
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-10-09 09:03:45 +0200

    Add header for mobile with glassmorphism

--- components/about/AboutCard.vue
+++ components/about/AboutCard.vue
@@ -7,7 +7,7 @@ defineProps({
 
 <template>
   <article
-    class="p-[1px] rounded-lg bg-gradient-to-r from-gray-300/40 via-gray-500/25 to-gray-600/20 shadow-md hover:shadow-lg transition-shadow duration-300"
+    class="p-[1px] rounded-lg bg-gradient-to-br lg:bg-gradient-to-r from-gray-300/40 via-gray-500/25 to-gray-600/20 shadow-md hover:shadow-lg transition-shadow duration-300"
   >
     <div class="bg-[#0a0a0a]/60 rounded-lg p-4">
       <h2 class="text-xl font-medium text-white mb-3 border-gray-500 pb-1">
