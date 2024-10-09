707aae9ad1ed138a0b8dd6d623f38c6ac4b9b64c
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-10-09 11:31:57 +0200

    Redesign with fonts and colors

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="lg:flex">
-    <About
-      class="lg:h-screen lg:sticky lg:top-0 border-b lg:border-r lg:border-b-0 border-separator pb-[4rem] lg:max-w-[50rem] lg:overflow-y-auto"
-    />
-    <main class="pt-8 pb-[5rem] lg:pb-0 lg:w-[50%]">
-      <Projects />
-    </main>
-    <Transition
-      enter-active-class="transition-transform duration-500 ease-out"
-      enter-from-class="translate-y-40"
-      enter-to-class="translate-y-0"
-      leave-active-class="transition-transform duration-500 ease-out"
-      leave-from-class="translate-y-0"
-      leave-to-class="translate-y-40"
-    >
-      <AboutHeaderMobile v-if="showHeader" class="md:hidden" />
-    </Transition>
+  <div>
+    <div class="lg:flex">
+      <About
+        class="lg:h-screen lg:sticky lg:top-0 border-b lg:border-r lg:border-b-0 border-separator pb-[4rem] lg:max-w-[50rem] lg:overflow-y-auto"
+      />
+      <main class="pt-8 pb-[5rem] lg:pb-0 lg:w-[50%]">
+        <Projects />
+      </main>
+      <client-only>
+        <Transition
+          enter-active-class="transition-transform duration-500 ease-out"
+          enter-from-class="translate-y-40"
+          enter-to-class="translate-y-0"
+          leave-active-class="transition-transform duration-500 ease-out"
+          leave-from-class="translate-y-0"
+          leave-to-class="translate-y-40"
+        >
+          <AboutHeaderMobile v-if="showHeader" class="md:hidden" />
+        </Transition>
+      </client-only>
+    </div>
   </div>
 </template>
 
@@ -25,14 +29,17 @@ import Projects from "~/components/projects/Projects.vue";
 import AboutHeaderMobile from "~/components/about/header/AboutHeaderMobile.vue";
 
 const showHeader = ref(false);
-
 onMounted(() => {
-  window.addEventListener("scroll", onScroll);
-  onScroll();
+  if (import.meta.client) {
+    window.addEventListener("scroll", onScroll);
+    onScroll();
+  }
 });
 
 onUnmounted(() => {
-  window.removeEventListener("scroll", onScroll);
+  if (import.meta.client) {
+    window.removeEventListener("scroll", onScroll);
+  }
 });
 
 const onScroll = () => {
