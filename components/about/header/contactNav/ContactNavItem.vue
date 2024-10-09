6443ca8e20737bed8fe52e3a1a22e01cf78a1d49
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-10-09 09:10:11 +0200

    Add header for mobile with glassmorphism

--- components/about/header/contactNav/ContactNavItem.vue
+++ components/about/header/contactNav/ContactNavItem.vue
@@ -1,11 +1,6 @@
 <template>
   <li
-    class="contact
-    flex items-center
-    justify-center w-[2.75rem]
-    h-[2.75rem] border
-    border-gray-500 text-gray-500 transition
-    hover:border-white hover:text-white rounded-full lg:w-[4.5rem] lg:h-[4.5rem]"
+    class="contact flex items-center justify-center w-[2.75rem] h-[2.75rem] border border-white/40 text-white/80 lg:text-white/55 transition lg:hover:border-white lg:hover:text-white rounded-full lg:w-[4.5rem] lg:h-[4.5rem]"
   >
     <slot />
   </li>
