0f8f4f1ec68a2bc31349817fd53f235674a126ac
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-10-09 08:09:51 +0200

    Add header for mobile with glassmorphism

---
+++ components/about/header/AboutHeaderMobile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fixed bottom-8 left-4 backdrop-blur bg-white/30 p-3 right-4 rounded-lg border border-white/20 shadow-xl shadow-white/10">
+    <div class="flex items-center justify-around">
+      <NuxtLink aria-label="github" href="https://github.com/manusanchev" target="_blank">
+        <GithubIcon class="w-5 h-5" />
+      </NuxtLink>
+      <ClientOnly fallbackTag="div">
+          <NuxtLink aria-label="email" href="mailto:manuvempleo@gmail.com" target="_blank">
+            <EmailIcon class="w-5 h-5"  />
+          </NuxtLink>
+        <template #fallback>
+            <EmailIcon class="w-5 h-5" />
+        </template>
+      </ClientOnly>
+      <NuxtLink aria-label="linkedin" href="https://www.linkedin.com/in/manusanchev/" target="_blank">
+        <LinkednIcon class="w-5 h-5" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import GithubIcon from "~/components/shared/icons/GithubIcon.vue";
+import EmailIcon from "~/components/shared/icons/EmailIcon.vue";
+import LinkednIcon from "~/components/shared/icons/LinkednIcon.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="lg:flex">
     <About class="lg:h-screen lg:sticky lg:top-0 border-b lg:border-r lg:border-b-0 border-separator pb-[4rem] lg:max-w-[50rem] lg:overflow-y-auto" />
-    <main class="pt-8 lg:w-[50%]">
+    <main class="pt-8 pb-[5rem] lg:w-[50%]">
       <Projects />
     </main>
+    <Transition enter-active-class="transition-transform duration-500 ease-out"
+                enter-from-class="translate-y-40"
+                enter-to-class="translate-y-0"
+                leave-active-class="transition-transform duration-500 ease-out"
+                leave-from-class="translate-y-0"
+                leave-to-class="translate-y-40">
+     <AboutHeaderMobile v-if="showHeader" class="md:hidden" />
+    </Transition>
   </div>
 </template>
+
+<script setup lang="ts">
+import About from "~/components/about/About.vue";
+import Projects from "~/components/projects/Projects.vue";
+import AboutHeaderMobile from "~/components/about/header/AboutHeaderMobile.vue";
+
+const showHeader = ref(false);
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+  onScroll();
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", onScroll);
+});
+
+const onScroll = () => {
+  const threshold = 100;
+  const scrollY = window.scrollY;
+  showHeader.value = scrollY > threshold;
+
+};
+</script>
