4b63b74d5fcb0f9a4f700877e059329a89968806
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2024-12-16 21:05:48 +0100

    add new design - wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        fixel: ["Fixel", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
-      },
-      colors: {
-        separator: "#262626",
-        title: "#cbd5e1",
+        geist: ["Geist", "Helvetica", "Arial", "sans-serif"],
       },
     },
   },
