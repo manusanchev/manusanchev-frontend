10e05ceeec0a4eccd3f77b5352b135feae522fe4
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2026-02-02 17:58:20 +0100

    util season.ts

--- src/utils/season.ts
+++ src/utils/season.ts
@@ -2,4 +2,5 @@ const today = new Date();
 const month = today.getMonth() + 1;
 const day = today.getDate();
 
-export const isChristmasSeason = (month === 12 && day >= 22) || (month === 1 && day <= 7);
+export const isChristmasSeason =
+  (month === 12 && day >= 22) || (month === 1 && day <= 7);
